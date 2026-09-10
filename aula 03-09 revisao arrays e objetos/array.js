08d6bc360f83ff7135daae9a1d582f278f918cbe
Gilberto V Oliveira <45500064+gvoliveira@users.noreply.github.com>
2026-09-10 15:18:12 -0300

    Add files via upload

--- aula 03-09 revisao arrays e objetos/array.js
+++ aula 03-09 revisao arrays e objetos/array.js
@@ -1,23 +1,25 @@
-//criando um array
-const arr = [2, 3, 4]
-let numero
-console.log(arr)
-arr.push(7) //adiciona o elemento
-console.log(arr)
-numero = arr.pop() //remove o elemento e retorna
-console.log(arr)
+/*Atividade - Faça um programa que crie e leia 5 valores de preços de produtos de uma loja. Após a leitura desses valores, percorra o vetor e imprima a soma de valores desses produtos. Além disso, imprima também o índice onde está o produto mais caro.
+Entrada - preços (5) array
+Processamento - soma dos produtos, encontrar índice mais caro
+Saída - resultados armazenados pelo processamento
+*/
+const precos = [];
+const tamanho = 5;
+for(let i = 0; i < tamanho; i++){
+    let preco = Number(prompt("Digite o preco do produto: "))
+    precos.push(preco)
+}
+let totalProdutos = 0.0, 
+precoMaisCaro = precos[0], 
+indiceMaisCaro = 0;
 
-//percorrer o array usando for
-for(let i = 0; i < arr.length; i++){
-    console.log(`O valor no índice ${i} é ${arr[i]} `)
+for(let i = 0; i < tamanho; i++){
+    totalProdutos += precos[i] 
+
+    if(precos[i] > precoMaisCaro){
+        precoMaisCaro = precos[i]
+        indiceMaisCaro = i
+    }
 }
 
-//percorrendo usando o for-of
-console.log("for-of: ")
-for(let valor of arr){
-    console.log(`O valor é ${valor}`)
-}
-
-console.log(valor)
-
-/*Atividade - Faça um programa que crie e leia 5 valores de preços de produtos de uma loja. Após a leitura desses valores, percorra o vetor e imprima a soma de valores desses produtos. Além disso, imprima também o índice onde está o produto mais caro.*/
+alert(`O valor total dos produtos é ${totalProdutos}. O produto mais caro está no índice ${indiceMaisCaro}`)
